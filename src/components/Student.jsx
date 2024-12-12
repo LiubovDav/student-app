c516261e02d3f44a1e6674f07556bf59f2c25cdc
Liubov Dav <lubasha0512@gmail.com>
2024-12-11 23:06:10 -0800

    Implemented

---
+++ src/components/Student.jsx
@@ -0,0 +1,34 @@
+import PropTypes from "prop-types";
+import './Student.css';
+
+
+const Student = (props) => {
+
+  const attendanceButtonClicked = () => {
+      // Invoke the function passed in through the prop named "onPresenceToggle"
+      // This function refers to the toggleStudentPresence function in App
+      props.onPresenceToggle(props.id);
+  };
+
+  const nameColor = props.isPresent ? 'green' : 'red';
+
+  return (
+      <div>
+          <ul>
+              <li className={nameColor}>Nickname: {props.name}</li>
+              <li>Email: {props.email}</li>
+          </ul>
+          <button onClick={attendanceButtonClicked}>Toggle if {props.name} is present</button>
+      </div>
+  );
+};
+
+Student.propTypes = {
+  id: PropTypes.number.isRequired,
+  name: PropTypes.string.isRequired,
+  email: PropTypes.string.isRequired,
+  isPresent: PropTypes.bool.isRequired,
+  onPresenceToggle: PropTypes.func.isRequired,
+};
+
+export default Student;
---
+++ src/components/StudentList.jsx
@@ -0,0 +1,39 @@
+import './StudentList.css';
+import PropTypes from 'prop-types';
+import Student from './Student';
+
+const StudentList = (props) => {
+  const studentComponents = props.students.map((student, index) => {
+      return (
+          <li key={index}>
+              <Student
+                  id={student.id}
+                  name={student.nameData}
+                  email={student.emailData}
+                  isPresent={student.isPresentData}
+                  onPresenceToggle={props.onStudentPresenceToggle}
+              ></Student>
+          </li>
+      );
+  });
+
+
+    return (
+        <section>
+            <h2 className="student-list__heading">Student List</h2>
+            <ul>{studentComponents}</ul>
+        </section>
+    );
+};
+
+StudentList.propTypes = {
+  students: PropTypes.arrayOf(PropTypes.shape({
+      id: PropTypes.number.isRequired,
+      nameData: PropTypes.string.isRequired,
+      emailData: PropTypes.string.isRequired,
+      isPresentData: PropTypes.bool.isRequired,
+  })),
+  onStudentPresenceToggle: PropTypes.func.isRequired,
+};
+
+export default StudentList;
